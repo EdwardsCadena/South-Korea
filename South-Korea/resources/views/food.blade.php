a31f138e4235e6ca9c0d4ea9533bdeb77d557d5c
Edward <earodriguez154@misena.edu.co>
2019-06-09 22:16:17 -0500

    refactor
    se añade vista food

--- South-Korea/resources/views/food.blade.php
+++ South-Korea/resources/views/food.blade.php
@@ -1,5 +1,172 @@
 @extends('layaouth.estructura')
 @section('contenido')
+    <div class="parallax-container valign-wrapper">
+        <div class="section no-pad-bot">
+            <div class="container">
+                <div class="row center">
+                    <h1 class="header center teal-text text-lighten-2">Corean Food</h1>
+                </div>
+            </div>
+        </div>
+        <div class="parallax"><img src="img/comida/inicio.jpg" alt="Unsplashed background img 2"></div>
+    </div>
 
-<p>djfbgjdfhgfjhdgfjhdgf</p>
-    @stop
+    <div class="container">
+        <div class="section">
+
+            <div class="row">
+                <div class="col s12 center">
+
+                    <h2 class="center brown-text">What will come to our table?</h2>
+                    <h6 class="left-align light">
+                        Korean cuisine is characterized by the wide variety of meats and fish used, as well as vegetables.
+                        Fermented and preserved foods are very common. They tend to be extremely spicy for western taste.
+                    </h6>
+                </div>
+            </div>
+            <div class="parallax-container valign-wrapper">
+
+                <div class="parallax"><img src="img/comida/dos.jpg" alt="Unsplashed background img 2"></div>
+            </div>
+        </div>
+    </div>
+
+            <div class="container">
+                <div class="section">
+
+                    <div class="row">
+                        <div class="col s12 center">
+
+                            <h2 class="center brown-text">Fermented food dishes</h2>
+                            <h6 class="left-align light">
+                                These fermented meals are part of the amount of dishes that are served at the table during a typical Korean meal.
+                                Everything arrives at the table at once, in small bowls.
+                            </h6>
+                            <h6 class="left-align light">
+                                These are some of the fermented foods
+                            </h6>
+                            <h6>
+                                <ul>
+                                    <li>Kimchi</li>
+                                    <li>Jeotgal</li>
+                                    <li>Doenjang</li>
+
+                                </ul>
+                            </h6>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="parallax-container valign-wrapper">
+                <div class="section no-pad-bot">
+                    <div class="container">
+
+                    </div>
+                </div>
+                <div class="parallax"><img src="img/comida/fermentacion.jpg" alt="Unsplashed background img 2"></div>
+            </div>
+    <div class="container">
+        <div class="section">
+
+            <div class="row">
+                <div class="col s12 center">
+
+                    <h2 class="center brown-text">Traditional Korean dishes</h2>
+                    <h6 class="left-align light">
+                        In addition to the fermented products, there are a number of delicious traditional
+                        dishes that can be eaten in South Korea.
+                        Some of them are very rich and you will want to eat them whenever possible.
+                        Others will be enough to try them once.
+                    </h6>
+
+                </div>
+            </div>
+            <div class="parallax-container valign-wrapper">
+
+                <div class="parallax"><img src="img/comida/tradicional.jpg" alt="Unsplashed background img 2"></div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="section">
+
+            <div class="row">
+                <div class="col s12 center">
+
+                    <h2 class="center brown-text">Here some of their most traditional dishes</h2>
+                    <div class="col s12 m6">
+                        <div class="icon-block">
+                            <h6 class="light">
+                            <ul>
+                                <li><b>Bibimbap</b></li>
+                                <li><b>Samgyetang</b></li>
+                                <li><b>Namul</b></li>
+                                <li><b>Samgyeopsal</b></li>
+                                <li><b>Bulgogi</b></li>
+                                <li><b>Galbi</b></li>
+                                <li><b>Bab</b></li>
+                            </ul>
+                            </h6>
+                        </div>
+                    </div>
+
+                    <div class="col s12 m6">
+                        <div class="icon-block">
+                            <h6 class="light">
+                            <ul>
+                                <li><b>Mandu</b></li>
+                                <li><b>Jokbal</b></li>
+                                <li><b>Gimbap</b></li>
+                                <li><b>Pajeon</b></li>
+                                <li><b>Beondegi</b></li>
+                                <li><b>Japchae</b></li>
+                                <li><b>Sannakji</b></li>
+                            </ul>
+                            </h6>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="parallax-container valign-wrapper">
+        <div class="parallax"><img src="img/comida/bebida.jpg" alt="Unsplashed background img 2"></div>
+    </div>
+    <div class="container">
+        <div class="section">
+
+            <div class="row">
+                <div class="col s12 center">
+
+                    <h2 class="center brown-text">Drinking in South Korea</h2>
+                    <h6 class="left-align light">
+                        As for the beers from South Korea, there are two brands that you will find everywhere: Cass and Hite.
+                        They are not expensive and can be found anywhere.
+                        If we look for typical drinks of the country, we will have to try <b>soju</b>, a liqueur made from
+                        rice extract that could be compared with vodka. The <b>maehwasu</b>, made with cirulas, is known as the feminine soju,
+                        because it is softer than this.
+                        There is a drink known as Korean rice wine: <b>makgeolli</b>. It's something sweet and milky-looking.
+                    </h6>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <div class="parallax-container valign-wrapper">
+        <div class="ui section">
+            <div class="ui container">
+                <br><br>
+                <h1 class="header center teal-text text-lighten-2">Advice</h1>
+                <div class="row center">
+                    <h5 class="header col s12 light">Do not drink tap water and you should avoid fish and shellfish from street stalls in summer.</h5>
+                </div>
+                <br><br>
+            </div>
+        </div>
+        <div class="parallax"><img src="img/comida/comidacallejera.jpg" alt="Unsplashed background img 2"></div>
+    </div>
+
+
+@stop
